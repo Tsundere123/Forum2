dbbe23cfc4a68bc997e7129c24eba33a1001e53e
strangers123 <91206077+Tsundere123@users.noreply.github.com>
2023-09-30 14:03:55 +0200

    Added account role list

---
+++ Forum2/Controllers/AccountRoleController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Forum2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forum2.Controllers;
+
+public class AccountRoleController : Controller
+{
+    private readonly AccountDbContext _accountDbContext;
+
+    public AccountRoleController(AccountDbContext accountDbContext)
+    {
+        _accountDbContext = accountDbContext;
+    }
+
+    public async Task<IActionResult> Table()
+    {
+        List<AccountRoles> accountRolesList = await _accountDbContext.AccountRoles.ToListAsync();
+        return View(accountRolesList);
+    }
+}
