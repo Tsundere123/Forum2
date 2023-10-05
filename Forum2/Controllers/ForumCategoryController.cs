75ec18f1e316dbc159626c0fd691569dddcfab80
strangers123 <91206077+Tsundere123@users.noreply.github.com>
2023-10-05 12:21:15 +0200

    Implemented forum category table view

---
+++ Forum2/Controllers/ForumCategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Forum2.Models;
+using Forum2.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forum2.Controllers;
+
+public class ForumCategoryController : Controller
+{
+    private readonly AccountDbContext _accountDbContext;
+    private readonly ForumDbContext _forumDbContext;
+    
+    public ForumCategoryController(AccountDbContext accountDbContext, ForumDbContext forumDbContext)
+    {
+        _accountDbContext = accountDbContext;
+        _forumDbContext = forumDbContext;
+    }
+    
+    public async Task<IActionResult> ForumCategoryTable()
+    {
+        List<ForumCategory> forumCategories = await _forumDbContext.ForumCategory.ToListAsync();
+        var forumCategoriesListViewModel = new ForumCategoryViewModel(forumCategories, "ForumCategoryTable");
+        return View(forumCategoriesListViewModel);
+    }
+}
---
+++ Forum2/ViewModels/ForumCategoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Forum2.Models;
+
+namespace Forum2.ViewModels
+{
+    public class ForumCategoryViewModel
+    {
+        public IEnumerable<ForumCategory> ForumCategories;
+        public string? CurrentViewName;
+
+        public ForumCategoryViewModel(IEnumerable<ForumCategory> forumCategories, string? currentViewName)
+        {
+            ForumCategories = forumCategories;
+            CurrentViewName = currentViewName;
+        }
+    }
+}
