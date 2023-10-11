ca986ed214d014be3ffb37e20599b7d44b731885
Cenosphere <git@cenosphe.re>
2023-10-11 14:13:58 +0200

    seed default users and roles

--- Forum2/DAL/InitDb.cs
+++ Forum2/DAL/InitDb.cs
@@ -1,4 +1,8 @@
-﻿namespace Forum2.Models;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Forum2.Models;
 
 public class InitDb
 {
@@ -48,6 +52,86 @@ public class InitDb
                 }
             };
             context.AddRange(accounts);
+            context.SaveChanges();
+        }
+
+        if (!context.Roles.Any())
+        {
+            context.Roles.Add(new ApplicationRole
+            {
+                Id = Guid.NewGuid().ToString(), 
+                Name = "Administrator", 
+                NormalizedName = "ADMINISTRATOR", 
+                Color = "#FF0000", 
+                IsFixed = true
+            });
+            context.Roles.Add(new ApplicationRole
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Moderator",
+                NormalizedName = "MODERATOR",
+                Color = "#0000FF",
+                IsFixed = true
+            });
+            context.SaveChanges();
+        }
+
+        if (!context.Users.Any())
+        {
+            var admin = new ApplicationUser
+            {
+                DisplayName = "Administrator",
+                Email = "admin@test.com",
+                NormalizedEmail = "ADMIN@TEST.COM",
+                UserName = "admin@test.com",
+                NormalizedUserName = "ADMIN@TEST.COM",
+                AvatarUrl = "https://art.pixilart.com/32cca92ae455838.png",
+                SecurityStamp = Guid.NewGuid().ToString("D")
+            };
+
+            var mod = new ApplicationUser
+            {
+                DisplayName = "Moderator",
+                Email = "mod@test.com",
+                NormalizedEmail = "MOD@TEST.COM",
+                UserName = "mod@test.com",
+                NormalizedUserName = "MOD@TEST.COM",
+                AvatarUrl = "https://art.pixilart.com/32cca92ae455838.png",
+                SecurityStamp = Guid.NewGuid().ToString("D")
+            };
+
+            var regular = new ApplicationUser
+            {
+                DisplayName = "User",
+                Email = "user@test.com",
+                NormalizedEmail = "USER@TEST.COM",
+                UserName = "user@test.com",
+                NormalizedUserName = "USER@TEST.COM",
+                AvatarUrl = "https://art.pixilart.com/32cca92ae455838.png",
+                SecurityStamp = Guid.NewGuid().ToString("D")
+            };
+            
+            var pass = new PasswordHasher<ApplicationUser>();
+            admin.PasswordHash = pass.HashPassword(admin, "Yes1234-");
+            mod.PasswordHash = pass.HashPassword(mod, "Yes1234-");
+            regular.PasswordHash = pass.HashPassword(regular, "Yes1234-");
+
+            context.Users.Add(admin);
+            context.Users.Add(mod);
+            context.Users.Add(regular);
+
+            context.UserRoles.Add(new IdentityUserRole<string>
+            {
+                RoleId = context.Roles.FirstOrDefault(r => r.Name == "Administrator")?.Id ?? throw new InvalidOperationException(),
+                UserId = admin.Id
+            });
+            
+            context.UserRoles.Add(new IdentityUserRole<string>
+            {
+                RoleId = context.Roles.FirstOrDefault(r => r.Name == "Moderator")?.Id ?? throw new InvalidOperationException(),
+                UserId = mod.Id
+            });
+            
             context.SaveChanges();
         }
     }
