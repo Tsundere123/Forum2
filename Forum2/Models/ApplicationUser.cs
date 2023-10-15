b820bced16c1c5af8be5321cf754563a358f4fe9
Cenosphere <git@cenosphe.re>
2023-10-15 13:22:03 +0200

    require displayname and avatar

--- Forum2/Models/ApplicationUser.cs
+++ Forum2/Models/ApplicationUser.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace Forum2.Models;
 
 public class ApplicationUser : IdentityUser
 {
+    [Required]
+    [MinLength(2)]
     public string DisplayName { get; set; } = string.Empty;
+    
+    [Required]
     public string? AvatarUrl { get; set; } = string.Empty;
 }
