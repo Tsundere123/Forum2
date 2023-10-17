d4bcf111a7586281f465ffdfa690f5ff0e78f1b3
Cenosphere <git@cenosphe.re>
2023-10-17 19:57:13 +0200

    add general search page, add specific search pages

---
+++ Forum2/Controllers/SearchController.cs
@@ -0,0 +1,84 @@
+﻿using Forum2.DAL;
+using Forum2.Models;
+using Forum2.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forum2.Controllers;
+
+public class SearchController : Controller
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IForumThreadRepository _threadRepository;
+    private readonly IForumPostRepository _postRepository;
+    
+    public SearchController(UserManager<ApplicationUser> userManager, IForumThreadRepository threadRepository, IForumPostRepository postRepository)
+    {
+        _userManager = userManager;
+        _threadRepository = threadRepository;
+        _postRepository = postRepository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index(string? query)
+    {
+        var viewModel = new SearchResultViewModel();
+        if (query == null) return View(viewModel);
+        
+        var threads = await _threadRepository.GetAll();
+        var posts = await _postRepository.GetAll();
+        var users = _userManager.Users.ToList();
+        
+        viewModel.Query = query;
+        viewModel.Threads = threads.Where(t => t.ForumThreadTitle.ToUpper().Contains(query.ToUpper())).ToList();
+        viewModel.Posts = posts.Where(p => p.ForumPostContent.ToUpper().Contains(query.ToUpper())).ToList();
+        viewModel.Users = users.Where(u => u.DisplayName.ToUpper().Contains(query.ToUpper())).ToList();
+
+        return View(viewModel);
+    }
+    
+    [HttpGet]
+    [Route("/Search/Threads")]
+    public async Task<IActionResult> Threads(string? query)
+    {
+        var viewModel = new SearchResultViewModel();
+        if (query == null) return View(viewModel);
+        
+        var threads = await _threadRepository.GetAll();
+        
+        viewModel.Query = query;
+        viewModel.Threads = threads.Where(t => t.ForumThreadTitle.ToUpper().Contains(query.ToUpper())).ToList();
+
+        return View(viewModel);
+    }
+    
+    [HttpGet]
+    [Route("/Search/Posts")]
+    public async Task<IActionResult> Posts(string? query)
+    {
+        var viewModel = new SearchResultViewModel();
+        if (query == null) return View(viewModel);
+        
+        var posts = await _postRepository.GetAll();
+        
+        viewModel.Query = query;
+        viewModel.Posts = posts.Where(p => p.ForumPostContent.ToUpper().Contains(query.ToUpper())).ToList();
+
+        return View(viewModel);
+    }
+    
+    [HttpGet]
+    [Route("/Search/Users")]
+    public async Task<IActionResult> Users(string? query)
+    {
+        var viewModel = new SearchResultViewModel();
+        if (query == null) return View(viewModel);
+        
+        var users = _userManager.Users.ToList();
+        
+        viewModel.Query = query;
+        viewModel.Users = users.Where(u => u.DisplayName.ToUpper().Contains(query.ToUpper())).ToList();
+
+        return View(viewModel);
+    }
+}
---
+++ Forum2/ViewModels/SearchResultViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Forum2.Models;
+
+namespace Forum2.ViewModels;
+
+public class SearchResultViewModel
+{
+    public string? Query { get; set; }
+    public List<ForumThread>? Threads { get; set; }
+    public List<ForumPost>? Posts { get; set; }
+    public List<ApplicationUser>? Users { get; set; }
+}
