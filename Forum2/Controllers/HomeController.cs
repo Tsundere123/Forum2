c15bcc57cf2b0be9836828a9ad52ef708143fd8c
Cenosphere <git@cenosphe.re>
2023-10-26 11:59:56 +0200

    add sliders for latest threads, posts, users

--- Forum2/Controllers/HomeController.cs
+++ Forum2/Controllers/HomeController.cs
@@ -1,21 +1,43 @@
 ﻿using System.Diagnostics;
+using Forum2.DAL;
 using Microsoft.AspNetCore.Mvc;
 using Forum2.Models;
+using Forum2.ViewModels;
 
 namespace Forum2.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IForumThreadRepository _forumThreadRepository;
+    private readonly IForumPostRepository _forumPostRepository;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IForumThreadRepository forumThreadRepository, IForumPostRepository forumPostRepository)
     {
         _logger = logger;
+        _forumThreadRepository = forumThreadRepository;
+        _forumPostRepository = forumPostRepository;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var threads = await _forumThreadRepository.GetAll();
+        var posts = await _forumPostRepository.GetAll();
+        
+        var latestThreads = (threads ?? Array.Empty<ForumThread>())
+            .Where(t => t.ForumThreadIsSoftDeleted == false)
+            .OrderByDescending(t => t.ForumThreadCreationTimeUnix).Take(5);
+        
+        var latestPosts = (posts ?? Array.Empty<ForumPost>())
+            .OrderByDescending(p => p.ForumPostCreationTimeUnix).Take(5);
+
+        var model = new HomeViewModel
+        {
+            ForumThreads = latestThreads.ToList(),
+            ForumPosts = latestPosts.ToList()
+        };
+        
+        return View(model);
     }
 
     public IActionResult Privacy()
---
+++ Forum2/ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Forum2.Models;
+
+namespace Forum2.ViewModels;
+
+public class HomeViewModel
+{
+    public List<ForumThread>? ForumThreads { get; set; }
+    public List<ForumPost>? ForumPosts { get; set; }
+}
