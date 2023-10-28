d5cc98b35d54b5123e67f718094632018210b9bb
Cenosphere <git@cenosphe.re>
2023-10-28 15:00:55 +0200

    update AccountController.cs to match views

--- Forum2/Controllers/AccountController.cs
+++ Forum2/Controllers/AccountController.cs
@@ -1,39 +1,15 @@
-﻿using Forum2.DAL;
-using Forum2.Models;
-using Forum2.ViewModels;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Forum2.Controllers;
 
 public class AccountController : Controller
 {
-    private readonly UserManager<ApplicationUser> _userManager;
-    public AccountController(UserManager<ApplicationUser> userManager)
+    public IActionResult Table()
     {
-        _userManager = userManager;
+        return View();
     }
-    
-    public async Task<IActionResult> Table()
+    public IActionResult Grid()
     {
-        var accounts = await _userManager.Users.ToListAsync();
-        var accountListviewModel = new AccountListViewModel(accounts, "Table");
-        return View(accountListviewModel);
+        return View();
     }
-    public async Task<IActionResult> Grid()
-    {
-        var accounts = await _userManager.Users.ToListAsync();
-        var accountListviewModel = new AccountListViewModel(accounts, "Grid");
-        return View(accountListviewModel);
-    }
-
-    public async Task<IActionResult> Details(string? accountId)
-    {
-        var account = await _userManager.FindByIdAsync(accountId);
-        // var account = await _accountRepository.GetAccountById(accountId);
-        if (account == null) return BadRequest("Account not found");
-        return View(account);
-    }
-    
 }
