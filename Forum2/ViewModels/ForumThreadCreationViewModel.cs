597c52caf59fddf4c4d3b0c9e155c1e905ec77d6
Cenosphere <git@cenosphe.re>
2023-10-28 20:20:50 +0200

    refactor getter setter for view models

--- Forum2/ViewModels/ForumThreadCreationViewModel.cs
+++ Forum2/ViewModels/ForumThreadCreationViewModel.cs
@@ -1,24 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using Forum2.Models;
+﻿using Forum2.Models;
 
 namespace Forum2.ViewModels;
 
 public class ForumThreadCreationViewModel
 {
-    public IEnumerable<ApplicationUser> Accounts;
-    public ForumCategory ForumCategory;
-    public ForumThread ForumThread;
-    public ForumPost ForumPost;
-
-    public ForumThreadCreationViewModel(ForumCategory forumCategory, ForumThread forumThread,
-        ForumPost forumPost,
-        IEnumerable<ApplicationUser> applicationUsers)
-    {
-        ForumCategory = forumCategory;
-        ForumThread = forumThread;
-        ForumPost = forumPost;
-        Accounts = applicationUsers;
-    }
-
+    public IEnumerable<ApplicationUser> Accounts { get; set; }
+    public ForumCategory ForumCategory { get; set; }
+    public ForumThread ForumThread { get; set; }
+    public ForumPost ForumPost { get; set; }
 }
